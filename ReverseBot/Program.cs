f55ec0bc3b6c72abc55d0e80f281b8ac67caf9b2
Lukas Kurz <vb.net@kurzweb.de>
2021-11-24 17:01:22 +0100

    Added ability to find more dll-paths

--- ReverseBot/Program.cs
+++ ReverseBot/Program.cs
@@ -2,5 +2,7 @@
 using Windows.Services.Store;
 
 Console.WriteLine(ReverseBot.WinRT.WinRT.FindImplementation<StoreContext>());
+Console.WriteLine(ReverseBot.WinRT.WinRT.FindImplementation<Windows.UI.Xaml.Controls.WebView>());
+Console.WriteLine(ReverseBot.WinRT.WinRT.FindImplementation<Windows.UI.Xaml.Controls.Page>());
 
 Console.ReadLine();
--- ReverseBot/WinRT/WinRT.cs
+++ ReverseBot/WinRT/WinRT.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.IO;
+using System.Linq;
 
 namespace ReverseBot.WinRT
 {
@@ -9,18 +11,23 @@ namespace ReverseBot.WinRT
 
         public static string FindImplementation(Type type)
         {
+            using (RegistryKey root = Registry.LocalMachine)
+            using (RegistryKey key = root.OpenSubKey($@"SOFTWARE\Microsoft\WindowsRuntime\ActivatableClassId\{type.FullName}"))
+                if (key != null && key.GetValueNames().Contains("DllPath"))
+                    return (string)key.GetValue("DllPath");
+
             Guid iid = type.GetInterface($"I{type.Name}").GUID;
             using (RegistryKey root = Registry.ClassesRoot)
             {
                 string clsid = null;
                 using (RegistryKey key = root.OpenSubKey($@"Interface\{{{iid}}}\ProxyStubClsid32"))
-                {
                     clsid = (string)key.GetValue(null);
-                }
+
+                if (clsid == null)
+                    throw new FileNotFoundException();
+
                 using (RegistryKey key = root.OpenSubKey($@"CLSID\{clsid}\InProcServer32"))
-                {
                     return (string)key.GetValue(null);
-                }
             }
         }
     }
