f79cf1ea945afd5a0d796c725afc4802d8dda38f
Lukas Kurz <lukas@shortdev.de>
2024-11-30 23:27:21 +0100

    Convert to cli

--- ReverseBot/Program.cs
+++ ReverseBot/Program.cs
@@ -2,127 +2,125 @@
 using ReverseBot.AppxActivation;
 using Spectre.Console;
 using System;
+using System.CommandLine;
 using System.ComponentModel;
 using System.Diagnostics;
-using Style = Spectre.Console.Style;
 
-while (true)
+RootCommand rootCommand = [];
+
+rootCommand.AddCommand(GuidCommand());
+rootCommand.AddCommand(HresCommand());
+rootCommand.AddCommand(DllSearch());
+rootCommand.AddCommand(AppxActivationCommand());
+rootCommand.AddCommand(WinRTImplCommand());
+
+return await rootCommand.InvokeAsync(args);
+
+static Command GuidCommand()
 {
-    Console.Clear();
-    var mode = AnsiConsole.Prompt(
-        new SelectionPrompt<string>()
-        {
-            HighlightStyle = Style.WithForeground(Color.LightGreen)
-        }
-        .Title("Select a module")
-        .AddChoices(new[] {
-            "GUID",
-            "HRES",
-            "DLL_SEARCH",
-            "Activate_Appx",
-            "WinRT_Impl",
-            "EXIT"
-        })
-    );
+    Command guidCommand = new("guid");
 
-    try
+    Argument<string> valueArg = new("value");
+    guidCommand.AddArgument(valueArg);
+
+    guidCommand.SetHandler(static input =>
     {
-        switch (mode)
+        Guid guid = new(input.Replace("GUID_", "").Replace("_", "-"));
+        Console.WriteLine(guid.ToString());
+        Console.WriteLine(guid.ToString("X")); // NDBPX
+
+        try
         {
-            case "GUID":
-                {
-                    string input = AnsiConsole.Ask<string>("Your [lightgreen]Guid[/]:");
-                    Guid guid = new(input.Replace("GUID_", "").Replace("_", "-"));
-                    Console.WriteLine(guid.ToString());
-                    Console.WriteLine(guid.ToString("X")); // NDBPX
-
-                    try
-                    {
-                        var impl = WinRTUtils.FindIIDImplementation(guid);
-                        Console.WriteLine($"Name: {impl.Name}; IID_{impl.Name}");
-                        Console.WriteLine($"DLL: {impl.DllPath}");
-                    }
-                    catch { }
-
-                    try
-                    {
-                        var impl = WinRTUtils.FindCLSIDImplementation(guid);
-                        Console.WriteLine($"Name: {impl.Name}; CLSID_{impl.Name}");
-                        Console.WriteLine($"DLL: {impl.DllPath}");
-                    }
-                    catch { }
-                    break;
-                }
-            case "HRES":
-                {
-                    string input = AnsiConsole.Ask<string>("Your [lightgreen]Hres[/]:");
-                    if (!int.TryParse(input, out int hRes))
-                        throw new ArgumentException("Not a valid integer");
-
-                    Win32Exception exceptionData = new(hRes);
-                    AnsiConsole.WriteLine(exceptionData.Message);
-                    break;
-                }
-            case "DLL_SEARCH":
-                {
-                    string dllName = AnsiConsole.Ask<string>("Your [lightgreen]DllName[/]:");
-
-                    Process[] processes = Process.GetProcesses();
-                    foreach (Process process in processes)
-                        try
-                        {
-                            foreach (ProcessModule module in process.Modules)
-                                if (module.FileName.ToLower().Contains(dllName.ToLower()))
-                                {
-                                    AnsiConsole.WriteLine("---");
-                                    AnsiConsole.WriteLine(
-                                        $"NAME: {process.ProcessName}\r\n" +
-                                        $"PID: {process.Id}\r\n" +
-                                        $"PATH: {process.MainModule.FileName}"
-                                    );
-                                    AnsiConsole.WriteLine("---");
-                                    break;
-                                }
-                        }
-                        catch
-                        {
-                            AnsiConsole.WriteLine($"Can't enumerate modules of \"{process.ProcessName}\". You might want to run as admin...");
-                        }
-                    break;
-                }
-            case "Activate_Appx":
-                {
-                    string appUserModelId = AnsiConsole.Ask<string>("[lightgreen]appUserModelId[/]:");
-                    string packageFullName = AnsiConsole.Ask<string>("[lightgreen]packageFullName[/]:");
-                    string debuggerPath = AnsiConsole.Ask<string>("[lightgreen]debuggerPath[/]:");
-
-                    ApplicationActivationManager.DisableDebugging(packageFullName);
-                    ApplicationActivationManager.EnableDebugging(packageFullName, debuggerPath);
-                    ApplicationActivationManager.ActivateApplication(appUserModelId);
-
-                    AnsiConsole.MarkupLine("Press [red]ENTER[/] to resume");
-                    Console.ReadLine();
-
-                    ApplicationActivationManager.DisableDebugging(packageFullName);
-                    break;
-                }
-            case "WinRT_Impl":
-                {
-                    string typeName = AnsiConsole.Ask<string>("[lightgreen]typeName[/]:");
-                    Console.WriteLine(WinRTUtils.QueryClassRegistration(typeName));
-                    break;
-                }
-            case "EXIT":
-                return;
-            default:
-                throw new ArgumentException("Wrong mode!");
+            var impl = WinRTUtils.FindIIDImplementation(guid);
+            Console.WriteLine($"Name: {impl.Name}; IID_{impl.Name}");
+            Console.WriteLine($"DLL: {impl.DllPath}");
         }
-    }
-    catch (Exception ex)
+        catch { }
+
+        try
+        {
+            var impl = WinRTUtils.FindCLSIDImplementation(guid);
+            Console.WriteLine($"Name: {impl.Name}; CLSID_{impl.Name}");
+            Console.WriteLine($"DLL: {impl.DllPath}");
+        }
+        catch { }
+    }, valueArg);
+    return guidCommand;
+}
+
+static Command HresCommand()
+{
+    Command guidCommand = new("hres");
+
+    Argument<int> valueArg = new("value");
+    guidCommand.AddArgument(valueArg);
+
+    guidCommand.SetHandler(static hres =>
     {
-        AnsiConsole.WriteException(ex);
-    }
-    AnsiConsole.WriteLine();
-    AnsiConsole.MarkupLine("Press [red]ENTER[/] to proceed");
-    Console.ReadLine();
+        Win32Exception exceptionData = new(hres);
+        AnsiConsole.WriteLine(exceptionData.Message);
+    }, valueArg);
+    return guidCommand;
+}
+
+static Command DllSearch()
+{
+    Command guidCommand = new("dll-search");
+
+    Argument<string> nameArg = new("name");
+    guidCommand.AddArgument(nameArg);
+
+    guidCommand.SetHandler(static dllName =>
+    {
+        Process[] processes = Process.GetProcesses();
+        foreach (Process process in processes)
+            try
+            {
+                foreach (ProcessModule module in process.Modules)
+                    if (module.FileName.ToLower().Contains(dllName.ToLower()))
+                    {
+                        AnsiConsole.WriteLine("---");
+                        AnsiConsole.WriteLine(
+                            $"NAME: {process.ProcessName}\r\n" +
+                            $"PID: {process.Id}\r\n" +
+                            $"PATH: {process.MainModule.FileName}"
+                        );
+                        AnsiConsole.WriteLine("---");
+                        break;
+                    }
+            }
+            catch
+            {
+                AnsiConsole.WriteLine($"Can't enumerate modules of \"{process.ProcessName}\". You might want to run as admin...");
+            }
+    }, nameArg);
+    return guidCommand;
+}
+
+static Command AppxActivationCommand()
+{
+    Command guidCommand = new("appx-activate");
+
+    Option<string> aumid = new("appUserModelId");
+    guidCommand.AddOption(aumid);
+
+    guidCommand.SetHandler(static appUserModelId =>
+    {
+        ApplicationActivationManager.ActivateApplication(appUserModelId);
+    }, aumid);
+    return guidCommand;
+}
+
+static Command WinRTImplCommand()
+{
+    Command guidCommand = new("winrt-impl");
+
+    Argument<string> typeNameArg = new("typeName");
+    guidCommand.AddArgument(typeNameArg);
+
+    guidCommand.SetHandler(static typeName =>
+    {
+        Console.WriteLine(WinRTUtils.QueryClassRegistration(typeName));
+    }, typeNameArg);
+    return guidCommand;
 }
